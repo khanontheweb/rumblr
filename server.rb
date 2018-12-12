9f5ef2ee0ebfea5a53b3b50b63b2bd8ffefa924a
Momo Khan <snillmomo@gmail.com>
2018-12-12 12:52:50 -0500

    User can now sign in. View their dash. User can now log out. Next is to let user make posts

--- server.rb
+++ server.rb
@@ -15,19 +15,41 @@ class Post < ActiveRecord::Base
 end
 
 get '/' do #CREATE
-  erb :home
+  if(session[:user_id].nil?) #if no user is logged in then give the option to sign up or login
+    erb :home
+  else
+    redirect '/users/dash' #if the user is logged in, show their dashboard
+  end
 end
 
-
+get '/users/dash' do #READ
+  @user = User.find(session[:user_id]) #grab the user by checking the value of user id in session hash
+  erb :'/users/dash'
+end
 
 get '/users/:id' do #READ
-  @user = User.find(params['id'])
+  @user = User.find(params['id']) #using user id instead of session hash for url purposes
   erb :'/users/profile'
 end
 
 post '/users/signup' do #CREATE
-  @user = User.new(fname: params['fname'], lname: params['lname'], email: params['email'], birthday: params['birthday'], password: params['password'])
-  @user.save
-  session[:user_id] = @user.id
-  redirect "/users/#{@user.id}"
+  @user = User.new(fname: params['fname'], lname: params['lname'], email: params['email'], birthday: params['birthday'], password: params['password']) #make the user
+  @user.save #save user to db
+  session[:user_id] = @user.id #this session belongs to this user
+  redirect "/users/dash" #show the users dash
+end
+
+post '/users/login' do #CREATE
+  user = User.find_by(email: params['email']) #find user by their email
+  if !user.nil? #check if the user even exists, if not we should be throwing an error hear FEEDBACK
+    if user.password == params['password'] #if the password matches, session belongs to user, show dash
+      session[:user_id] = user.id
+      redirect '/users/dash'
+    end
+  end
+end
+
+post '/users/logout' do #CREATE
+  session[:user_id] = nil
+  redirect '/'
 end
